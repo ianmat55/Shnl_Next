2ddae39abbe10aca6a78b7001f8198f77cb18f0a
ianmat55 <ianmat55@gmail.com>
2022-02-10 11:55:30 -0800

    lifestyle title added

--- pages/lifestyle.js
+++ pages/lifestyle.js
@@ -1,5 +1,20 @@
+import styles from '../styles/Lifestyle.module.css'
+
+const Hero = () => {
+	return(
+		<div id={styles.lifestyleHero}>
+			<h1 id={styles.lifestyleTitle}> Waikiki/ <br /> Downtown/ <br /> Nature </h1>
+			<img src='https://via.placeholder.com/160x230' id={styles.img1} />
+			<img src='https://via.placeholder.com/100x140' id={styles.img2} />
+			<img src='https://via.placeholder.com/120x150' id={styles.img3} />
+		</div>
+	)
+};
+
 export default function Lifestyle() {
 	return (
-		<h1> Lifestyle </h1>
+		<div className='tablet:content'>
+			<Hero />
+		</div>
 	)
 }

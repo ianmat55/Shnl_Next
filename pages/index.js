46322741dc49cba34c082efcacd434b722eaf4a9
ianmat55 <ianmat55@gmail.com>
2022-02-18 11:42:43 -0800

    issue with finding width fixed

--- pages/index.js
+++ pages/index.js
@@ -12,70 +12,78 @@ import desktop2 from '../public/assets/desktop_images/lifestyle.jpg'
 import desktop3 from '../public/assets/desktop_images/contact.jpg'
 import desktop4 from '../public/assets/desktop_images/about.jpg'
 
-const BackgroundImages = ({ mobileImages, desktopImages }) => {
-	const [count, setCount] = useState(0);
-  	
-	let width = () => window.innerWidth;
+const useWindowSize = () => {
+	const [windowSize, setWindowSize] = useState({
+		width: undefined,
+		height: undefined,
+	});
 
 	useEffect(() => {
-		const incrementCount = setInterval(() => setCount(count === mobileImages.length - 1 ? 0 : count + 1), 9000);
+		function handleResize() {
+			setWindowSize({
+				width: window.innerWidth,
+				height: window.innerHeight,
+			});
+		}
+		window.addEventListener('resize', handleResize);
+		handleResize();
+		return () => window.removeEventListener('resize', handleResize);
+	}, []); // empty array ensures effect is only run on inital render
+
+	return windowSize;
+}
+
+const BackgroundImages = ({ images, size }) => {
+	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		const incrementCount = setInterval(() => setCount(count === images.length - 1 ? 0 : count + 1), 9000);
 		return () => clearInterval(incrementCount);
 	}, [count]);
-	console.log(width);
-	const transform = {
-		transform: `translateX(-${width * count}px)`,
-		transition: 'transform 0.6s ease-in-out',
-		position: 'relative',
-		width: '100vw',
-		height: '100vh'
-	}
 
-	if (width) {
-		return (
-			<div style={transform}>
-				{mobileImages.map((image, index) => {	
-					return (
-						<div key={`img${index}`}>
-							<Image 
-								src={image} 
-								alt={`img${index}`}
-								layout='fill'
-								className={styles.imageWrapper}
-							/>
-						</div>		
-				)
-				})}
-			</div>	
-		)
-	} else {
-		return (
-			<>
-				{desktopImages.map((image, index) => {	
-					return (
-						<div className={styles.backgroundContainer}>
-							<Image
-								src={image} 
-								alt='idk'
-								key={index}
-								layout='fill'
-								className={styles.imageWrapper}
-							/>			
-						</div>		
-					)
-				})}
-			</>	
-		)
+	const transform = {
+		// transform: `translateX(-${size.width * count}px)`,
+		// transition: 'transform 0.6s ease-in-out',
+		position: 'relative',
+		display: 'flex',
+		height: '100vh',
+		width: `${size.width * images.length}`
 	}
+	console.log(size.width)
+	return (	
+		<div style={transform}>
+			{images.map((image, index) => {	
+				return (
+					<div className={styles.imageWrapper} key={`img${index}`}>
+						<Image 
+							src={image} 
+							alt={`img${index}`}
+							className={styles.imageWrapper}
+							layout='fill'
+						/>
+					</div>		
+			)
+			})}
+		</div>
+	)
 } 
 
 
 export default function Home() {
 	const mobileImages = [img1, img2, img3, img4];
 	const desktopImages = [desktop1, desktop2, desktop3, desktop4];
+	const size = useWindowSize();	
+	let background
 
+	if (size.width >= 800) {
+		background = <BackgroundImages images={desktopImages} size={size} />
+	} else {
+		background = <BackgroundImages images={mobileImages} size={size} />
+	};
+	
 	return (
 		<section id={styles.home}>
-			<BackgroundImages mobileImages={mobileImages} desktopImages={desktopImages} />
+			{background}
 		</section>
 	)
 };

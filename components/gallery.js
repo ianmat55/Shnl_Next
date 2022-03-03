472001492309b5dc5d672a33177d7e5ba3aa72bc
ianmat55 <ianmat55@gmail.com>
2022-03-02 20:38:38 -0800

    added key to li element, should compile now

--- components/gallery.js
+++ components/gallery.js
@@ -10,9 +10,9 @@ export default function Gallery({ images }) {
 				let source = `https://api.shanelhonolulu.com/assets/${image}`
 				// let source = `http://localhost:8055/assets/${image}`
 				return (
-					<li className={styles.photoContainer}>
+					<li key={index} className={styles.photoContainer}>
 						{/* <Image className={styles.photo} key={index} loader={() => source} src={image} layout='fill' objectFit='cover' sizes='20vw' /> */}
-						<img key={index} src={source} className={styles.photo} />
+						<img src={source} className={styles.photo} alt={`img${index}`} />
 					</li>
 				)
 			})}

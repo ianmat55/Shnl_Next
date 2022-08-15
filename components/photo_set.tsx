7d4b874ee0e5a51674ecc807e0f327619319ac23
ianmat55 <ianmat55@gmail.com>
2022-08-15 09:31:33 -1000

    replaced image w/ next Image component in photo_set.tsx

--- components/photo_set.tsx
+++ components/photo_set.tsx
@@ -1,15 +1,18 @@
 import styles from '../styles/gallery.module.css'
+import Image from 'next/image'
 
 export default function PhotoSet({ images }: any) {
 	return (
 		<ul id={styles.gallery}>
 			{images.map((image: any, index: number) => {
-				const source = `https://api.shanelhonolulu.com/assets/${image}`
 
 				return (
 					<li key={index} className={styles.photoContainer}>
-						{/* eslint-disable-next-line @next/next/no-img-element */}
-						<img src={source} className={styles.photo} alt={`img${index}`} />
+							<div className={styles.caption}> 
+							</div>
+							<Image src={image} 
+									loader={({src}) => `https://api.shanelhonolulu.com/assets/${src}`} 
+									alt={`img${index}`} objectFit='cover' layout='fill' />
 					</li>
 				)
 			})}

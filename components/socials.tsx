ea3812abd8faabf563cfa6901cb8d14f1225bbeb
ianmat55 <ianmat55@gmail.com>
2022-09-29 20:46:56 -0700

    installed linter, added cool hover effect to social icons

--- components/socials.tsx
+++ components/socials.tsx
@@ -7,28 +7,28 @@ export default function SocialIcons() {
 	return (
 		<ul className={styles.iconContainer}>
 			<li className={styles.social_icon_wrapper} id={styles.instaIcon}>
+				<FontAwesomeIcon className={styles.sIcon} icon={faInstagram} size='sm'/>
 				<div className={styles.handle_wrapper}>
 					<p className={styles.handle}>
 						shanellnahs
 					</p>
 				</div>
-				<FontAwesomeIcon className={styles.sIcon} icon={faInstagram} size='sm'/>
 			</li>
 			<li className={styles.social_icon_wrapper} id={styles.fbIcon}>
+				<FontAwesomeIcon className={styles.sIcon} icon={faFacebook} size='sm'/>
 				<div className={styles.handle_wrapper}>
 					<p className={styles.handle}>
 						shanellnahs
 					</p>
 				</div>
-				<FontAwesomeIcon className={styles.sIcon} icon={faFacebook} size='sm'/>
 			</li>
 			<li className={styles.social_icon_wrapper} id={styles.emailIcon}>
+				<FontAwesomeIcon className={styles.sIcon} icon={faEnvelope} size='sm'/>
 				<div className={styles.handle_wrapper}>
 					<p className={styles.handle}>
 						shanellnahs
 					</p>
 				</div>
-				<FontAwesomeIcon className={styles.sIcon} icon={faEnvelope} size='sm'/>
 			</li>
 		</ul>
 	)

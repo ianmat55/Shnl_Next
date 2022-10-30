8f9a4bb74a31fafb29cc65dff8daa61d0f4db02f
ianmat55 <ianmat55@gmail.com>
2022-10-29 17:44:16 -0700

    changing format of home page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,120 +1,125 @@
-import Link from 'next/link'
-import styles from '../styles/navbar.module.css'
+import Link from "next/link";
+import styles from "../styles/navbar.module.css";
 
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { useState } from 'react';
-import SocialIcons from './socials';
+import { useState } from "react";
+import SocialIcons from "./socials";
 
 interface MenuLink {
-	name: string,
-	link: string
+  name: string;
+  link: string;
 }
 
 interface SocialIcons {
-	insta: any,
-	facebook: any,
-	email: any
+  insta: any;
+  facebook: any;
+  email: any;
 }
 
 interface MenuItemsProps {
-	menuLinks: MenuLink[]
+  menuLinks: MenuLink[];
 }
 
-// NEED TO REFACTOR 
+// NEED TO REFACTOR
 
 const MenuItems = ({ menuLinks }: MenuItemsProps) => {
-	const links = menuLinks.map((item: any, index: number) =>
-		<Link key={index} href={item.link} passHref={true}>
-			<li> <p className={styles.menuItem}> {item.name} </p> </li>
-		</Link>
-	);
+  const links = menuLinks.map((item: any, index: number) => (
+    <Link key={index} href={item.link} passHref={true}>
+      <li>
+        {" "}
+        <p className={styles.menuItem}> {item.name} </p>{" "}
+      </li>
+    </Link>
+  ));
 
-	function closeMenu() {
-		const navigation = document.querySelector('nav');
-		const toggleButton = document.querySelector('#toggleIcon') as HTMLElement
-		const width = navigation!.clientWidth;
-		if (width < 1000) {
-			navigation!.style.transition = 'transform 0.3s ease-in-out';
-			navigation!.style.transform = 'translateX(-300px)';
-			toggleButton!.style.transform = 'translateX(0px)';
-		}
-	}
+  function closeMenu() {
+    const navigation = document.querySelector("nav");
+    const toggleButton = document.querySelector("#toggleIcon") as HTMLElement;
+    const width = navigation!.clientWidth;
+    if (width < 1000) {
+      navigation!.style.transition = "transform 0.3s ease-in-out";
+      navigation!.style.transform = "translateX(-300px)";
+      toggleButton!.style.transform = "translateX(0px)";
+    }
+  }
 
-	return (
-		<>
-			<nav id='translate-nav'>
-				<Link key='home' href='/' passHref={true}>
-					<h1 id={styles.logo}> ShanelHonolulu </h1>
-				</Link>
-				<ul onClick={closeMenu}>
-					{links}
-				</ul>
-				<div id={styles.nav_social_icons}>
-					<SocialIcons />
-				</div>
-			</nav>
-		</>
-	);
+  return (
+    <>
+      <nav id="translate-nav">
+        <Link key="home" href="/" passHref={true}>
+          <h1 id={styles.logo}> ShanelHonolulu </h1>
+        </Link>
+        <ul onClick={closeMenu}>{links}</ul>
+        <div id={styles.nav_social_icons}>
+          <SocialIcons />
+        </div>
+      </nav>
+    </>
+  );
 };
 
 const HamburgerMenu = ({ sideBar, setSideBar }: any) => {
-	
-	function menuToggle() {
-		const navigation = document.querySelector('nav')
-		const toggleButton = document.querySelector('#toggleIcon') as HTMLElement
-		const transition = 'transform 0.3s ease-in-out'
-		if (sideBar) {
-			navigation!.style.transform = 'translateX(-300px)'
-			toggleButton!.style.transform = 'translateX(0px)'
-			setSideBar(false)
-		} else {
-			navigation!.style.transform = 'translateX(0px)'
-			toggleButton!.style.transform = 'translateX(300px)'
-			setSideBar(true)
-		}
-		navigation!.style.transition = transition
-		toggleButton!.style.transition = transition
-	}
+  function menuToggle() {
+    const navigation = document.querySelector("nav");
+    const toggleButton = document.querySelector("#toggleIcon") as HTMLElement;
+    const transition = "transform 0.3s ease-in-out";
+    if (sideBar) {
+      navigation!.style.transform = "translateX(-300px)";
+      toggleButton!.style.transform = "translateX(0px)";
+      setSideBar(false);
+    } else {
+      navigation!.style.transform = "translateX(0px)";
+      toggleButton!.style.transform = "translateX(300px)";
+      setSideBar(true);
+    }
+    navigation!.style.transition = transition;
+    toggleButton!.style.transition = transition;
+  }
 
-	return(
-		<div id='toggleIcon' className={styles.hamburgerContainer}> 
-			<FontAwesomeIcon onClick={menuToggle} id={styles.hamburger} icon={faBars} size='sm' />
-		</div>
-	)
+  return (
+    <div id="toggleIcon" className={styles.hamburgerContainer}>
+      <FontAwesomeIcon
+        onClick={menuToggle}
+        id={styles.hamburger}
+        icon={faBars}
+        size="sm"
+      />
+    </div>
+  );
 };
 
 export default function Nav() {
-	const menuLinks = [
-		{
-			name: 'Love',
-			link: '/love'
-		},
-		{
-			name: 'Lifestyle',
-			link: '/lifestyle'
-		},
-		{
-			name: 'Local',
-			link: '/local',
-		},
-		{
-			name: 'Film',
-			link: '/film',
-		},
-		{
-			name: 'Contact',
-			link: '/contact'
-		}
-	];
+  const menuLinks = [
+    {
+      name: "Love",
+      link: "/love",
+    },
+    {
+      name: "Lifestyle",
+      link: "/lifestyle",
+    },
+    {
+      name: "Local",
+      link: "/local",
+    },
+    {
+      name: "Film",
+      link: "/film",
+    },
+    {
+      name: "Contact",
+      link: "/contact",
+    },
+  ];
 
-	const [sideBar, setSideBar] = useState(false)
+  const [sideBar, setSideBar] = useState(false);
 
-	return (
-		<>
-			<HamburgerMenu sideBar={sideBar} setSideBar={setSideBar} />
-			<MenuItems menuLinks={menuLinks} />
-		</>
-	)
-};
+  return (
+    <>
+      <HamburgerMenu sideBar={sideBar} setSideBar={setSideBar} />
+      <MenuItems menuLinks={menuLinks} />
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,138 @@
-import Image from 'next/image'
-import React, { useState, useEffect } from 'react'
-import styles from '../styles/Home.module.css'
+import Image from "next/image";
+import React, { useState } from "react";
+import styles from "../styles/Home.module.css";
 
-import mobile1 from '../public/assets/mobile_images/img1.jpg'
-import mobile2 from '../public/assets/mobile_images/img2.jpg'
-import mobile3 from '../public/assets/mobile_images/img3.jpg'
-import mobile4 from '../public/assets/mobile_images/img4.jpg'
+import mobile1 from "../public/assets/mobile_images/img1.jpg";
+import mobile2 from "../public/assets/mobile_images/img2.jpg";
+import mobile3 from "../public/assets/mobile_images/img3.jpg";
+import mobile4 from "../public/assets/mobile_images/img4.jpg";
 
-import desktop1 from '../public/assets/desktop_images/home1.jpeg'
-import desktop2 from '../public/assets/desktop_images/home2.jpeg'
-import desktop3 from '../public/assets/desktop_images/home3.jpg'
-import desktop4 from '../public/assets/desktop_images/home4.jpeg'
-import desktop5 from '../public/assets/desktop_images/home5.jpeg'
-import desktop6 from '../public/assets/desktop_images/wedding.jpg'
-import desktop7 from '../public/assets/desktop_images/home6.jpeg' // this is a good one
-import { useWindowSizeContext } from 'utils/context'
-import { Size } from 'shared/types'
-import SocialIcons from 'components/socials'
+import desktop1 from "../public/assets/desktop_images/home1.jpeg";
+import desktop2 from "../public/assets/desktop_images/home4.jpeg";
+import desktop3 from "../public/assets/desktop_images/home5.jpeg";
+import desktop4 from "../public/assets/desktop_images/wedding.jpg";
+import desktop5 from "../public/assets/desktop_images/home6.jpeg";
+import { useWindowSizeContext } from "utils/context";
+import { Size } from "shared/types";
+import SocialIcons from "components/socials";
+import Link from "next/link";
 
 interface BackgroundImagesProps {
-	images: string[],
-	size: Size
+  background: any[];
+  size: Size;
 }
 
-const BackgroundImages = ({ images, size }: BackgroundImagesProps) => {
-	const [count, setCount] = useState(0)
+const BackgroundImages = ({ background, size }: BackgroundImagesProps) => {
+  const [count, setCount] = useState(0);
 
-	useEffect(() => {
-		const incrementCount = setInterval(() => setCount(count === images.length - 1 ? 0 : count + 1), 11000);
-		return () => clearInterval(incrementCount)
-	}, [count, images.length]);
+  const incrementCount = (int: number) => {
+    console.log(int);
+    if (count == 0 && int < 0) {
+      return;
+    }
+    setCount(count === background.length - 1 ? 0 : count + int);
+  };
 
-	return (	
-		<div style={{
-			transform: `translateX(-${size!.width * count}px)`, 
-			transition: 'transform .8s ease-in-out', 
-			position: 'relative',
-			display: 'flex',
-			height: '100vh',
-			width: `${size!.width * images.length}`
-		}}>
-			{images.map((image, index) => {	
-				return (
-					<div className={styles.imageWrapper} key={`img${index}`}>
-						<Image 
-							src={image} 
-							alt={`img${index}`}
-							className={styles.imageWrapper}
-							layout='fill' />
-					</div>		
-			)
-			})}
-		</div>
-	)
-} 
-
-export default function Home() {
-	const mobileImages: string[] = [mobile1.src, mobile2.src, mobile3.src, mobile4.src]
-	const desktopImages: string[] = [desktop1.src, desktop2.src, desktop3.src, desktop4.src, desktop5.src, desktop6.src, desktop7.src]
-
-	const windowSize: Size = useWindowSizeContext()
-
-	let background
-	if (windowSize!.width >= 800) {
-		background = <BackgroundImages images={desktopImages} size={windowSize} />
-	} else {
-		background = <BackgroundImages images={mobileImages} size={windowSize} />
-	};
-	
-	return (
-		<section id={styles.home}>
-			{/* <div id="header_container">
-				<h1 id={styles.mainTitle} className='homeTitle'> Lifestyle, <br/> Business, <br/> & Wedding <br/> Photography </h1>
-			</div> */}
-			{background}
-			<div id={styles.home_social_icons}>
-				<SocialIcons />
-			</div>
-		</section>
-	)
+  return (
+    <div id={styles.homeBackgrounds}>
+      <div id={styles.arrows}>
+        <div className="leftArrow">
+          <svg
+            onClick={() => incrementCount(-1)}
+            width="24"
+            height="24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            transform="scale(-1.5,1.5)"
+            stroke="black"
+            stroke-width="0.5"
+          >
+            <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
+          </svg>
+        </div>
+        <div className="rightArrow">
+          <svg
+            onClick={() => incrementCount(1)}
+            width="24"
+            height="24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            transform="scale(1.5,1.5)"
+            stroke="black"
+            stroke-width="0.5"
+          >
+            <path d="M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z" />
+          </svg>
+        </div>
+      </div>
+      <div
+        style={{
+          transform: `translateX(-${size!.width * count}px)`,
+          transition: "transform .8s ease-in-out",
+          position: "relative",
+          display: "flex",
+          height: "100vh",
+          width: `${size!.width * background.length}`,
+        }}
+      >
+        {background.map((item, index) => {
+          return (
+            <div className={styles.imageWrapper} key={`img${index}`}>
+              <div className={styles.header_container}>
+                <Link key={index} href={item.link} passHref={true}>
+                  <h1 className={styles.hero_title}> {item.title} </h1>
+                </Link>
+              </div>
+              <Image
+                src={item.image}
+                alt={`img${index}`}
+                className={styles.imageWrapper}
+                layout="fill"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
+export default function Home() {
+  const mobileImages = [
+    { image: mobile1.src, title: "Shnl Photography", link: "/" },
+    { image: mobile2.src, title: "Love", link: "/love" },
+    { image: mobile3.src, title: "Lifestyle", link: "/lifestyle" },
+    { image: mobile4.src, title: "Local", link: "/local" },
+  ];
+  const desktopImages = [
+    { image: desktop1.src, title: "Shnl Photography", link: "/" },
+    { image: desktop2.src, title: "Love", link: "/love" },
+    { image: desktop3.src, title: "Lifestyle", link: "/lifestyle" },
+    { image: desktop4.src, title: "Local", link: "/local" },
+    { image: desktop5.src, title: "Film", link: "/film" },
+  ];
 
+  const windowSize: Size = useWindowSizeContext();
 
+  let background;
+  if (windowSize!.width >= 800) {
+    background = (
+      <BackgroundImages background={desktopImages} size={windowSize} />
+    );
+  } else {
+    background = (
+      <BackgroundImages background={mobileImages} size={windowSize} />
+    );
+  }
+
+  return (
+    <section id={styles.home}>
+      {background}
+      <div id={styles.home_social_icons}>
+        <SocialIcons />
+      </div>
+    </section>
+  );
+}

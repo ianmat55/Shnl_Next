dbab5527debfb078242aa537c812cc752317f6f4
ianmat55 <ianmat55@gmail.com>
2022-12-20 19:38:15 -1000

    prettier fix

--- components/header.tsx
+++ components/header.tsx
@@ -1,17 +1,18 @@
-import Image from 'next/image'
+import Image from "next/image";
+import React from "react";
 
 interface HeaderProps {
-    title: string
-    image: any
+  title: string;
+  image: string;
 }
 
 export default function Header({ title, image }: HeaderProps) {
-    return (
-        <div className='headerSection'>
-            <div className="headerImage">
-                <Image src={image} />
-            </div>
-            <h1 className='title'> {title} </h1>
-        </div>
-    )
+  return (
+    <div className="headerSection">
+      <div className="headerImage">
+        <Image src={image} />
+      </div>
+      <h1 className="title"> {title} </h1>
+    </div>
+  );
 }
--- pages/love/index.tsx
+++ pages/love/index.tsx
@@ -1,8 +1,10 @@
 import axios from "axios";
 import Gallery from "components/gallery";
-import { GalleryProps } from "../../shared/types";
+import { GalleryProps, Size } from "../../shared/types";
 import Header from "components/header";
 import wedding_desktop from "../../public/assets/wedding.jpg";
+import wedding_mobile from "../../public/assets/wedding_mobile.jpg";
+import { useWindowSizeContext } from "utils/context";
 
 export async function getStaticProps() {
   const res = await axios("https://api.shanelhonolulu.com/items/Love");
@@ -14,7 +16,8 @@ export async function getStaticProps() {
 }
 
 export default function Wedding({ images }: GalleryProps) {
-  const heroImage = wedding_desktop;
+  const windowSize: Size = useWindowSizeContext();
+  const heroImage = windowSize!.width >= 650 ? wedding_desktop : wedding_mobile;
   return (
     <section className="content">
       <Header title="WEDDING PHOTOGRAPHY" image={heroImage} />
